5fba6e03584a17533fe138b6e37890f68a0a2055
kanukanun <kanukanun@gmail.com>
2015-09-01 19:17:24 +0900

    added

--- MakeGableRoofVolumes/Loop.cs
+++ MakeGableRoofVolumes/Loop.cs
@@ -3,38 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Rhino;
 using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Display;
+using Rhino.DocObjects;
 
 namespace MakeGableRoofVolumes
 {
     class Loop : Rhino_Processing
     {
-        List<AstroObject> astroobjs = new List<AstroObject>();
+        Curve[] crv;
 
         public override void Setup() // this runs once in the beginning.
         {
-            astroobjs.Add(new AstroObject(63.71, new Point3d(), 1496.0, 365.4 * 10));
-            astroobjs.Add(new AstroObject(17.37, astroobjs[0].Center(), 384.0, 27.32 * 10));
-            astroobjs.Add(new AstroObject(17.37, astroobjs[1].Center(), 384.0, 27.32 * 10));
+            ObjectType.Curve;
+            RhinoGet.GetMultipleObjects("select rectangle", true, , crv);
+
+            for (int i = 0; i < crv.Length; i++)
+            {
+                crv[i].Z = 300;
+            }
         }
 
         public override void Draw() // this repeats until it ends
         {
-            astroobjs[0].Rotate();
-
-            for (int i = 1; i < astroobjs.Count; i++) {
-                astroobjs[i].UpdateCenter(astroobjs[i - 1].Center());
-                astroobjs[i].Rotate();
-
+            for (int i = 0; i < crv.Length; i++)
+            {
+                doc.Objects.AddPoint(crv[i]);
             }
-
-            foreach(AstroObject astroobj in astroobjs){
-                astroobj.Display(doc);
-            }
-
-        }
-        
         }
     }
-
+}
